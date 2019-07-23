f9246d0079f7933e35e9a85b45cbc91b8ecedc00
Thomas Birtwistle <tb@logicevo.io>
2019-07-23 07:31:37 +0100

    ContactImport model created for file upload

---
+++ UploadApplication/Models/ContactImport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UploadApplication.Models
+{
+    public class ContactImport
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string EmailAddress { get; set; }
+        public string Department { get; set; }
+        public string ContactType { get; set; }
+        public string CompanyName { get; set; }
+        public string BusinessPhone { get; set; }
+        public string Address1Street1 { get; set; }
+        public string Address1Street2 { get; set; }
+        public string Address1City { get; set; }
+        public string Address1PostalCode { get; set; }
+        public string Address1Country { get; set; }
+
+
+    }
+}
